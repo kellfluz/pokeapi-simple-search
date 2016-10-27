bd36c0983ded8e6e6d038ed4c6600a9faaf3527a
DaniloAgostinho <daniloagostinhosilva@gmail.com>
2016-10-27 08:37:12 -0200

    reforação js parte 1

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
 (function () {
   'use strict';
   
+  var searchInput = document.getElementById('search');
   var btn = document.getElementById('btn');
-  btn.disabled = true;
-  btn.onclick = reqListener;
-  var searchInput = document.getElementById('search');//pegei o campo de busca
-  
 
   searchInput.addEventListener('keypress', checkInput);//add evento de escuta
-
-
+  btn.addEventListener('click', reqListener);
 
   function checkInput() {
-    var searchValue = searchInput.value;
-    var rmSpaces = searchValue.trim();
+    var searchValue = searchInput.value.trim();
 
-    if (rmSpaces.length >= 2) {
-        document.getElementById('btn').disabled = false;
-        document.getElementById('btn').style.background = "#1e275b";
-        document.getElementById('btn').style.color = "#fff";
+    if (searchValue.length >= 2) {
+        btn.disabled = false;
+        btn.style.background = "#1e275b";
+        btn.style.color = "#fff";
     }
 
-        console.log(rmSpaces);
+    console.log(searchValue);
 
   }//end checkInput
 
