56a95c8b00abf3fd3446cd5579f64f123f2ada0c
DaniloAgostinho <daniloagostinhosilva@gmail.com>
2016-10-29 00:44:25 -0200

    files update request

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
   var searchInput = document.getElementById('search');
   var btn = document.getElementById('btn');
 
-  searchInput.addEventListener('keypress', checkInput);//add evento de escuta
-  btn.addEventListener('click', reqListener);
+  searchInput.addEventListener('keypress', checkInput);
+  btn.addEventListener('click', restSearch);
 
   function checkInput() {
     var searchValue = searchInput.value.trim();
 
-    if (searchValue.length >= 2) {
+    if (searchValue.length >= 4) {
         btn.disabled = false;
         btn.style.background = "#1e275b";
         btn.style.color = "#fff";
@@ -21,12 +21,35 @@
   }//end checkInput
 
 
-    function reqListener() {
-      var xhr =  new XMLHttpRequest();
-      xhr.open('GET', 'http://pokeapi.co/api/v2/pokemon/1/', true);
-      xhr.send();
-      console.log(xhr);
-    }
+
+  function restSearch() {
+    
+    var xhttp = new XMLHttpRequest();
+
+  xhttp.open("POST", "http://pokeapi.co/api/v2/pokemon/", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("name=" + searchPokemon);
+
+  console.log(xhttp);
+
+   
+  }
+
+  function log(message) {
+    var searchPokemon = searchInput.value.trim();
+    var client = new XMLHttpRequest();
+    client.open("POST", "/log");
+    client.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    client.send();
+}
+
+  //http://pokeapi.co/api/v2/pokemon/
+
+
+
+
+  
+
 
 })();
 
