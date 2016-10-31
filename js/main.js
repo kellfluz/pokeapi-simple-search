714ce5ec14886ac3ddfd84a634d86dac2c728622
@danilodev <daniloagostinhosilva@gmail.com>
2016-10-31 11:02:41 -0500

    solucionar problemas de cors

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
   var btn = document.getElementById('btn');
 
   searchInput.addEventListener('keypress', checkInput);
-  btn.addEventListener('click', restSearch);
+  btn.addEventListener('click', resBusca);
 
   function checkInput() {
     var searchValue = searchInput.value.trim();
@@ -20,24 +20,25 @@
 
   }//end checkInput
 
-  function restSearch() {
-    var restValue = searchInput.value.trim();
-    var urlRest = 'http://pokeapi.co/api/v2/pokemon/';
-    var result = urlRest += restValue;
 
-    var restPokemon = new XMLHttpRequest();
-    restPokemon.open('GET', result, true);
-    restPokemon.send();
-    restPokemon.addEventListener('readystatechange', function(){
-      if (restPokemon.readyState === 4) {
-        var response = restPokemon.JSON.parse(restPokemon);
-        console.log(response); 
-      } 
+  function resBusca() {
+    var valorDigitadoBusca = searchInput.value.trim();
+    var urlApi = 'http://pokeapi.co/api/v2/pokemon/';
+    var resBusca = urlApi += valorDigitadoBusca;
+    console.log(resBusca);
+
+    var requestApi = new XMLHttpRequest();
+    requestApi.open('GET', resBusca, true);
+    requestApi.send();
+    requestApi.addEventListener('onreadystatechange', function(){
+      if (requestApi.readystate === 4 && requestApi.status === 200) {
+        console.log(JSON.parse(requestApi.responseText));
+        console.log(requestApi.status);
+      }
     }, false);
-     
+    
+
   }
-
-
 
   //http://pokeapi.co/api/v2/pokemon/
 
