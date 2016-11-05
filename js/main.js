d14a883e447a3b92593eabda0ccff71e27ad1658
@danilodev <daniloagostinhosilva@gmail.com>
2016-11-05 10:07:03 -0200

    add novo main.js e gitignore

--- js/main.js
+++ js/main.js
@@ -11,49 +11,24 @@
     var $height = doc.getElementById('height');
 
     $searchField.addEventListener('input', validateField);
-    $button.addEventListener('click', function() {
-    seekPokemon();
-    }, false);
+    $button.addEventListener('click', seekPokemon, false);
 
     function validateField() {
       var enteredValue = $searchField.value.trim();
 
-      if (enteredValue.length >= 2) {
-        button.disabled = false;
-        button.style.background = "#1e275b";
-        button.style.color = "#fff";
+      function styleForm() {
+        if (enteredValue.length >= 2) {
+            button.disabled = false;
+            button.style.background = "#1e275b";
+            button.style.color = "#fff";
+        }
       }
+
+      styleForm();
 
     }
 
     function seekPokemon() {
-      var enteredValue = $searchField.value.trim();
-      var urlApi = 'http://pokeapi.co/api/v2/pokemon/' + enteredValue;
-
-      //rest API
-      var requestApi = new XMLHttpRequest();
-      requestApi.open('GET', urlApi, true);
-      requestApi.send();
-
-      requestApi.onreadystatechange = function() {
-        if (requestApi.readyState === 4 && requestApi.status === 200) {
-
-          var response = JSON.parse(requestApi.responseText);
-
-          console.log(response.length);
-          $name.innerHTML = response.name;
-          alert("Name → " + response.name);
-          alert("ID → " + response.id);
-          alert("Experiência → " + response.base_experience);
-          alert("Height → " + response.height);
-          alert("Weight → " + response.weight);
-          alert("Habilidade → " + response.abilities[0].ability.name);
-          alert("Tipo → " + response.types[0].type.name);
-          alert("Movimento → " + response.moves[0].move.name);
-
-        }
-
-      }
 
     }
 
