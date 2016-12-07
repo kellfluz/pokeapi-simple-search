9ff1fc44dae596bde2e9d2a3fb130547695f3fe8
dasilva08 <dasilva08>
2016-12-07 13:49:45 -0200

    desabititando o botão busca ao clicar

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
     var $searchField = doc.getElementById('search');
     var $button = doc.getElementById('button');
+    var $form = doc.forms.pokedex;
     var b = doc.querySelector('body');
 
     var $name = doc.getElementById('name');
@@ -11,7 +12,14 @@
     var $height = doc.getElementById('height');
 
     $searchField.addEventListener('input', validateField);
-    $button.addEventListener('click', seekPokemon);
+    $form.addEventListener('submit', seekPokemon);
+
+    function clearSearch() {
+        $button.disabled = true;
+        $button.style.background = "#fff";
+        $button.style.color = "#1d3c73";
+        $searchField.value = '';
+    }
 
     //function validate
     function validateField() {
@@ -22,15 +30,18 @@
             $button.disabled = false;
             $button.style.background = "#1e275b";
             $button.style.color = "#fff";
+        } else if (enteredValue.length == 0) {
+          clearSearch();
         }
       }
 
       styleForm();//exec funtion style form
-
     }
 
     //request for API
-    function seekPokemon() {
+    function seekPokemon(event) {
+      event.preventDefault();
+
       var url =  'http://pokeapi.co/api/v2/pokemon/' + $searchField.value.trim();
       var xhr = new XMLHttpRequest();
       xhr.open('GET', url, true);
@@ -44,16 +55,10 @@
           $name.append(response.name);
           $id.append(response.id);
           $experience.append(response.base_experience);
+          console.log(response);
         }
 
         showResult(xhr);
-
-        function clearSearch() {
-          $button.disabled = true;
-          $button.style.background = "#fff";
-          $button.style.color = "#1d3c73";
-          $searchField.value = '';
-        }
 
         clearSearch();
       }//end getResult
